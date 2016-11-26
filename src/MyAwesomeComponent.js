871c870e0008c8c84f1a5ce587bed3a4089045bc
andrerpena <andrerpena@gmail.com>
2016-11-26 18:14:05 -0200

    Initial commit

---
+++ src/MyAwesomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyAwesomeComponent extends Component {
+    render() {
+        return (
+            <div>
+                <button type="submit">Submit 2</button>
+            </div>
+        );
+    }
+}
+
+export default MyAwesomeComponent;
