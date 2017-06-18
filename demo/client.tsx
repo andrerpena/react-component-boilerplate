8f61468c56a7e6cbe019ebd6d19101f147843c3d
André Pena <andrerpena@gmail.com>
2017-06-18 17:49:56 +0100

    Styles are now loading

--- demo/client.tsx
+++ demo/client.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import MyAwesomeComponent from '../src/MyAwesomeComponent';
+require('./styles.scss');
 render(
     <div>
         <MyAwesomeComponent>
